391b41b0f0d2bddc63aa897c21a5c7cd7a79fd5f
Mikhail Kostrikov <kostrikovma@vk.com>
2022-11-17 12:37:15 +0300

    Added func number to text

--- script.js
+++ script.js
@@ -86,9 +86,10 @@ function gameStart() {
   inputMax.setAttribute("readonly", "");
 
   answer = Math.floor((startValue + endValue) / 2);
+  numberToText(answer);
   let message = `
   Вы загадали число
-  ${String(answer)} ?`;
+  ${String(answer)}?`;
   messageForUser(message);
 };
 
@@ -133,3 +134,100 @@ function gameRestart() {
   endValue = parseInt(inputMax.value);
   welcomeMessage();
 };
+
+function numberToText(number, strAnswer) {
+let strSign = "";
+let arrUnits = [
+  "один",
+  "два",
+  "три",
+  "четыре",
+  "пять",
+  "шесть",
+  "семь",
+  "восемь",
+  "девять",
+];
+let arrTensSpecial = [
+  "десять",
+  "одиннадцать",
+  "двенадцать",
+  "тринадцать",
+  "четырнадцать",
+  "пятнадцать",
+  "шестнадцать",
+  "семнадцать",
+  "восемнадцать",
+  "девятнадцать",
+];
+let arrTens = [
+  "двадцать",
+  "тридцать",
+  "сорок",
+  "пятьдесят",
+  "шестьдесят",
+  "семьдесят",
+  "восемьдесят",
+  "девяносто",
+];
+let arrHundreds = ["сто", "двести", "триста", "четыреста"];
+
+if (number !== 0) {
+  if (number < 0) {
+    strSign = "минус";
+  } else {
+    strSign = "";
+  }
+
+  let thirdPart = Math.floor(Math.abs(number) / 100) % 10;
+  let secondPart = Math.floor(Math.abs(number) / 10) % 10;
+  let firstPart = Math.abs(number) % 10;
+
+  if (!thirdPart && secondPart === 1) {
+    strAnswer = `${strSign} ${tensSpecial(firstPart)}`;
+  } else {
+    strAnswer = `${strSign} ${hundreds(thirdPart)} ${tens(secondPart)} ${units(
+      firstPart
+    )}`;
+  }
+} else {
+  strAnswer = "0";
+}
+
+function hundreds(num, strNum) {
+  if (num > 0 && num < 5) {
+    strNum = arrHundreds[num - 1];
+  } else if (num >= 5 && num <= 9) {
+    strNum = `${arrUnits[num - 1]}сот`;
+  } else {
+    strNum = "";
+  }
+  return strNum;
+}
+
+function tensSpecial(num, strNum) {
+  strNum = arrTensSpecial[num];
+  return strNum;
+}
+
+function tens(num, strNum) {
+  if (num >= 2 && num <= 9) {
+    strNum = arrTens[num - 2];
+  } else {
+    strNum = "";
+  }
+  return strNum;
+}
+
+function units(num, strNum) {
+  if (num > 0 && num <= 9) {
+    strNum = arrUnits[num - 1];
+  } else {
+    strNum = "";
+  }
+  return strNum;
+}
+strAnswer.replace(/\s+/g, " ").trim();
+strAnswer.length < 20 ? answer = strAnswer : answer;
+return answer;
+}
